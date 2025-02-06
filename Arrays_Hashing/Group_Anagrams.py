dc0fb7f5b231b02fc78c02fcc5a2f163c54eba08
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2025-02-06 01:16:20 -0500

    context question

--- Arrays_Hashing/Group_Anagrams.py
+++ Arrays_Hashing/Group_Anagrams.py
@@ -1,6 +1,17 @@
 from typing import List
 from collections import defaultdict
 
+"""
+Group Anagrams
+
+Given an array of strings strs, group all anagrams together into sublists. 
+You may return the output in any order. 
+
+An anagram is a string that contains the exact same characters as another string, 
+but the order of the characters can be different.
+"""
+
+
 class Solution:
     def groupAnagramsSorted(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
